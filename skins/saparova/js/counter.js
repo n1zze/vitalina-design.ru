fcb3d9c0639e872558296d172f4907118683e065
Alexandr Romashkevich <alexnizze@gmail.com>
2026-08-03 20:54:59 +0300

    Add interior-design service page, redesign about page sections, rebrand SAPAROVA to VITALINA DESIGN

--- skins/saparova/js/counter.js
+++ skins/saparova/js/counter.js
@@ -3,12 +3,15 @@ var h = setInterval(function () {
    $(".c.active").each(function () {
       var c = +$(this).data("current") || 0;
       var max = +$(this).data("max");
+      var suffix = $(this).data("suffix") || "";
       if (++c <= max) {
-         $(this).data("current", c).text(c);
-      } else $(this).removeClass("active");
+         $(this).data("current", c).text(c.toLocaleString("ru-RU") + suffix);
+      } else {
+         $(this).text(max.toLocaleString("ru-RU") + suffix);
+         $(this).removeClass("active");
+      }
    });
    if (!$(".c.active").length) {
       clearInterval(h);
-      console.log("the end");
    }
-}, 50);
+}, 30);
